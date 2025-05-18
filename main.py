0bfd8dfb10527d8a546c1f960371f211f96c1d54
Norwood91 <robnorwood91@gmail.com>
2025-05-18 00:21:05 -0700

    Program refactored and completed. Updated README.

--- main.py
+++ main.py
@@ -1,81 +1,129 @@
 import random
-
-print("Welcome to the Higher Lower Game!")
-print("In this game, you must try to guess which famous person has more followers than the other.")
-print("The goal is to get as many correct and accumulate the highest score.")
-print("Get one wrong and the game is over...")
-
-# ASK USER TO PLAY THE GAME
-play_game_or_quit = input("Are you ready to start? Type 'yes' to start or 'q' to quit: ").lower()
-
-score = 0
+import os
+import time
 
 # CREATE A LIST OF OBJECTS TO STORE PEOPLE TO COMPARE
-people_to_compare = [{"name":"Rihanna", "title": "Musician", "followers": 1000000},{"name":"Beyonce", "title": "Musician", "followers": 2000}, {"name": "GaryVee","title" : "Motivator", "followers": 15990}, {"name":"Donald Trump", "title": "President", "followers": 7000000}, {"name": "Usher", "title":"Musician", "followers": 10000000}, {"name": "Lebron James", "title": "Basketball Player", "followers": 20000000}]
+people_to_compare = [
+    {"name":"Rihanna", "title": "Musician", "followers": 100000},
+    {"name":"Beyonce", "title": "Musician", "followers": 20000}, 
+    {"name": "GaryVee","title" : "Motivator", "followers": 15990}, 
+    {"name":"Donald Trump", "title": "President", "followers": 700000}, 
+    {"name": "Usher", "title":"Musician", "followers": 1000000}, 
+    {"name": "Lebron James", "title": "Basketball Player", "followers": 2000000}, 
+    {"name": "Kim Kardashian", "title": "Business Woman", "followers": 5000000},
+    {"name": "Robert Norwood", "title": "Software Engineer", "followers": 500}, 
+    {"name": "Barack Obama", "title": "Former President", "followers": 4100300},
+    {"name": "Cristiano Ronaldo", "title": "Footballer", "followers": 65200000},
+    {"name": "Leo Messi", "title": "Footballer", "followers": 50500000},
+    {"name": "Selena Gomez", "title": "Musician", "followers": 42100000},
+    {"name": "The Rock", "title": "Actor and Wrestler", "followers": 39400000},
+    {"name": "Kylie Jenner", "title": "Reality Star", "followers": 39300000},
+    {"name": "Ariana Grande", "title": "Musician", "followers": 37600000},
+    {"name": "Nike", "title": "Sportswear Brand", "followers": 30100000},
+    {"name": "Justin Bieber", "title": "Musician", "followers": 29400000},
+    ]
 
-# RANDOMLY CHOOSE A PERSON FROM THE LIST, TO COMPARE
-def choose_random_person(list):
-    """Randomly chooses and returns two people from the list, inside a tuple"""
-    new_person_one = random.choice(list)
-    new_person_two = random.choice(list)
-
-    if new_person_one["name"] == new_person_two["name"]:
-        new_person_two = random.choice(list)
-
-    return new_person_one, new_person_two
-
-def create_screen_space():
-    """Creates screen space using print statements"""
+# GREET USER
+def greet_user():
     print()
-    print()
-    print()
-    print()
-    print()
-    print()
-    print()
+    print("Welcome to the Higher Lower Game!")
+    
+# CREATE SCREEN SPACE
+def create_space():
+    print("\n" * 2)
+    
+# RANDOMLY SELECT TWO PEOPLE FROM THE LIST TO COMPARE
+def random_person(exclude=None):
+    person = random.choice(people_to_compare)
+    while person == exclude:
+        person = random.choice(people_to_compare)
+    return person
 
+# FORMAT THE PEOPLES' FOLLOWER COUNT WITH A COMMA
+def format_followers(followers):
+    return format(followers, ",")
 
-# CREATE TWO PEOPLE
-person_one, person_two = choose_random_person(people_to_compare)
+# CLEAR THE SCREEN (For Cross-Platform)
+def clear_screen():
+    time.sleep(2)
 
-create_screen_space()
+    if os.name == 'nt': # Windows
+        os.system('cls')
+    else:
+        os.system('clear')
 
-# DISPLAY THE PEOPLE TO THE USER
-print(f"Person A is: {person_one["name"]}, who is a famous {person_one["title"]}, from the United States")
+# INITIALIZE SCORE
+score = 0
 
-create_screen_space()
+# INITIALIZE PERSON A/PERSON B
+person_a = random_person()
+person_b = random_person(exclude=person_a)
 
-print(f"Person B is: {person_two["name"]}, who is a famous {person_two["title"]}, from the United States")
+# FORMAT THE FOLLOWER COUNT
+person_a_followers = person_a["followers"]
+pa_formatted_followers = format_followers(person_a_followers)
 
-create_screen_space()
+person_b_followers = person_b["followers"]
+pb_formatted_followers = format_followers(person_b_followers)
 
-# ASK THE USER WHICH PERSON THEY THINK HAS MORE FOLLOWERS
-user_choice = input("Who do you think has more followers? Type 'a' or 'b': ").lower()
+greet_user()
+create_space()
 
+print(f"Person A is: {person_a['name']}, a {person_a['title']} from the United States, with {pa_formatted_followers} followers.")
 
-# CHECK TO SEE IF THE USER'S CHOICE IS CORRECT OR NOT
+create_space()
+print(f"Person B is: {person_b['name']}, a {person_b['title']} from the United States, with ____ followers.")
 
-# END GAME IF THE USER IS INCORRECT
+# MAIN GAME LOOP
+while True:
+    create_space()
 
-# INCREMENT THE SCORE VARIABLE IF THE USER IS CORRECT 
+    correct_answer = 'a' if person_a_followers > person_b_followers else 'b'
 
-# PICK A NEW PERSON FOR THE USER TO COMPARE THE PREVIOUS PERSON THEY GOT CORRECT TO
+    user_guess = input("Who do you think has more followers? \nEnter either 'a' or 'b' or 'q' to quit: ").strip().lower()
+    
+    if user_guess == "a" and correct_answer == "a":
+        print("THAT'S CORRECT!")
+        score+=1
+        print(f"YOUR SCORE IS: {score}")
+        person_a = person_a
+        person_a_followers = person_a["followers"]
+        pa_formatted_followers = format_followers(person_a_followers)
 
+        person_b = random_person(exclude=person_a)
+        person_b_followers = person_b["followers"]
+        pb_formatted_followers = format_followers(person_b_followers)
 
+        print("RESETTING SCREEN")
+        clear_screen()
 
+        print(f"Person A is: {person_a['name']}, a {person_a['title']} from the United States, with {pa_formatted_followers} followers.")
+        create_space()
+        print(f"Person B is: {person_b['name']}, a {person_b['title']} from the United States, with ____ followers.")
 
+    elif user_guess == "b" and correct_answer == "b":
+        print("THAT'S CORRECT!")
+        score+=1
+        print(f"YOUR SCORE IS: {score}")
+        person_a = person_b
+        person_a_followers = person_a["followers"]
+        pa_formatted_followers = format_followers(person_a_followers)
 
+        person_b = random_person(exclude=person_a)
+        person_b_followers = person_b["followers"]
+        pb_formatted_followers = format_followers(person_b_followers)
 
+        print("RESETTING SCREEN")
+        clear_screen()
 
+        print(f"Person A is: {person_a['name']}, a {person_a['title']} from the United States, with {pa_formatted_followers} followers.")
+        create_space()
+        print(f"Person B is: {person_b['name']}, a {person_b['title']} from the United States, with ____ followers.")
 
-
-
-
-
-
-
-
-# if person_one == person_two:
-#     person_two = choose_random_person(people_to_compare)
-# elif person_one["followers"] > person_two["followers"]:
-#     print(f"{person_one["name"]} has {person_one["followers"] - person_two["followers"]} more followers than {person_two["name"]}!")
+    elif user_guess == "q":
+        print("YOU CHOSE TO QUIT. GOODBYE!")
+        print(f"YOUR FINAL SCORE WAS: {score}")
+        break
+    else:
+        print(f"That's INCORRECT!\nGAME OVER.\nYOUR FINAL SCORE WAS {score}")
+        break
